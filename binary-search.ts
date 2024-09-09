bfb35111a0240847c840abfaf857679509051f7e
Joe Robin <joerobin26@gmail.com>
2024-09-09 12:40:40 +0530

    done some stuff

--- binary-search.ts
+++ binary-search.ts
@@ -19,7 +19,7 @@ derived => input lenght
 
 */
 
-import { TestCase } from './types'
+import { TestCase } from './types';
 
 const testCases: TestCase[] = [
   // Empty Array
@@ -221,7 +221,7 @@ const testCases: TestCase[] = [
   //   target: 1000001,
   //   expected: -1,
   // },
-]
+];
 
 // This worked even thought its wrong because if the target is at the first half of the array this will work like a linear search and search
 // every element from the end to the start
@@ -239,137 +239,198 @@ const testCases: TestCase[] = [
 // Math.trunc(end - start / 2):pointer 4
 // result:  4
 
-// This is basicaly a reverse linear search
-function _binarySearch(array: number[], target: number): number {
-  console.log(array, target)
-  let pointer: number = -1
-  let start: number = 0
-  let end: number = array.length // Adding -1 here results in a infinite loop
+// // This is basicaly a reverse linear search
+// function _binarySearch(array: number[], target: number): number {
+//   console.log(array, target);
+//   let pointer: number = -1;
+//   let start: number = 0;
+//   let end: number = array.length; // Adding -1 here results in a infinite loop
 
-  while (start <= end) {
-    pointer = Math.trunc(end - start / 2)
-    if (target === array[pointer]) return pointer
-    if (target > array[pointer]) start = pointer + 1
-    end = pointer - 1
-  }
+//   while (start <= end) {
+//     pointer = Math.trunc(end - start / 2);
+//     if (target === array[pointer]) return pointer;
+//     if (target > array[pointer]) start = pointer + 1;
+//     end = pointer - 1;
+//   }
 
-  return -1
-}
-// Bug
-function recursionSearch(array: number[], target: number) {
-  let pointer: number = -1
-  let start: number = 0
-  let end: number = array.length
+//   return -1;
+// }
+// // Bug
+// function recursionSearch(array: number[], target: number) {
+//   let pointer: number = -1;
+//   let start: number = 0;
+//   let end: number = array.length;
 
-  pointer = Math.trunc(end - start / 2)
-  if (target === array[pointer]) return pointer
-  if (target > array[pointer])
-    return recursionSearch(array.slice(start + 1, end + 1), target)
-  else return recursionSearch(array.slice(start, end), target)
-}
-// Bug
-function __search(nums: number[], target: number): number {
-  let infinityCheck = 0
-  let l = 0
-  let r = nums.length
-  let m = -1
+//   pointer = Math.trunc(end - start / 2);
+//   if (target === array[pointer]) return pointer;
+//   if (target > array[pointer])
+//     return recursionSearch(array.slice(start + 1, end + 1), target);
+//   else return recursionSearch(array.slice(start, end), target);
+// }
+// // Bug
+// function __search(nums: number[], target: number): number {
+//   let infinityCheck = 0;
+//   let l = 0;
+//   let r = nums.length;
+//   let m = -1;
 
-  while (l <= r) {
-    m = Math.floor((r - l) / 2)
+//   while (l <= r) {
+//     m = Math.floor((r - l) / 2);
 
-    if (target === nums[m]) return m
+//     if (target === nums[m]) return m;
 
-    if (target > nums[m]) {
-      l = m + 1
-    }
+//     if (target > nums[m]) {
+//       l = m + 1;
+//     }
 
-    if (target < nums[m]) {
-      r = m - 1
-    }
-    if (infinityCheck > 20) break
-    infinityCheck++
-  }
+//     if (target < nums[m]) {
+//       r = m - 1;
+//     }
+//     if (infinityCheck > 20) break;
+//     infinityCheck++;
+//   }
 
-  return -1
-}
+//   return -1;
+// }
 
-// Bug
-function __binarySearch(array: number[], target) {
-  const l = array.length
-  let start = 0
-  let end = l - 1
-  let m = -1
+// // Bug
+// function __binarySearch(array: number[], target) {
+//   const l = array.length;
+//   let start = 0;
+//   let end = l - 1;
+//   let m = -1;
 
-  let counter = 0
+//   let counter = 0;
 
-  while (start <= end) {
-    m = Math.trunc((end - start) / 2)
-    if (array[m] === target) return m
-    else if (target > array[m]) start = m + 1
-    else if (target < array[m]) end = m - 1
-    else {
-      return -1
-    }
-  }
+//   while (start <= end) {
+//     m = Math.trunc((end - start) / 2);
+//     if (array[m] === target) return m;
+//     else if (target > array[m]) start = m + 1;
+//     else if (target < array[m]) end = m - 1;
+//     else {
+//       return -1;
+//     }
+//   }
 
-  return -1
-}
+//   return -1;
+// }
 
-// Correct
-function binarySearch(input: number[], t: number): number {
-  let l = 0
-  let r = input.length - 1
-  let m = -1
+// // Correct
+// function binarySearch(input: number[], t: number): number {
+//   let l = 0;
+//   let r = input.length - 1;
+//   let m = -1;
 
-  while (l <= r) {
-    m = l + Math.trunc((r - l) / 2)
+//   while (l <= r) {
+//     m = l + Math.trunc((r - l) / 2);
 
-    if (input[m] === t) return m
-    if (t > input[m]) l = m + 1
-    else if (t < input[m]) r = m - 1
-    else return -1
-  }
+//     if (input[m] === t) return m;
+//     if (t > input[m]) l = m + 1;
+//     else if (t < input[m]) r = m - 1;
+//     else return -1;
+//   }
 
-  return -1
-}
+//   return -1;
+// }
 
-// const result = binarySearch([1, 3, 5, 7, 9], 5)
-// console.log('result: ', result)
+// // const result = binarySearch([1, 3, 5, 7, 9], 5)
+// // console.log('result: ', result)
 
-function binarySearch__(arr, x) {
-  let l = 0
-  let r = arr.length - 1
-  let mid = -1
-  while (r >= l) {
-    mid = l + Math.floor((r - l) / 2) // Missed this every step of the way
+// function binarySearch__(arr, x) {
+//   let l = 0;
+//   let r = arr.length - 1;
+//   let mid = -1;
+//   while (r >= l) {
+//     mid = l + Math.floor((r - l) / 2); // Missed this every step of the way
 
-    // If the element is present at the middle
-    // itself
-    if (arr[mid] == x) return mid
+//     // If the element is present at the middle
+//     // itself
+//     if (arr[mid] == x) return mid;
 
-    // If element is smaller than mid, then
-    // it can only be present in left subarray
-    if (arr[mid] > x) r = mid - 1
-    // Else the element can only be present
-    // in right subarray
-    else l = mid + 1
-  }
+//     // If element is smaller than mid, then
+//     // it can only be present in left subarray
+//     if (arr[mid] > x) r = mid - 1;
+//     // Else the element can only be present
+//     // in right subarray
+//     else l = mid + 1;
+//   }
 
-  // We reach here when element is not
-  // present in array
-  return -1
-}
+//   // We reach here when element is not
+//   // present in array
+//   return -1;
+// }
 
-// if (false)
+// const binarySEARCH = (num: number[], t: number): number => {
+//   let l = 0;
+//   let r = num.length - 1;
+//   let m = -1;
+
+//   while (l <= r) {
+//     m = Math.floor(l + (r - l) / 2);
+
+//     if (num[m] > t) r = m - 1;
+//     else if (num[m] < t) l = m + 1;
+//     else return m;
+//   }
+
+//   return -1;
+// };
+
+// const binarySeARCH = (nums: number[], l: number, r: number, t: number) => {
+//   if (l > r) return -1;
+
+//   let m = l + Math.floor(r - l / 2);
+
+//   if (nums[m] === t) return m;
+
+//   if (nums[m] > t) {
+//     r = r = m - 1;
+//     return binarySeARCH(nums, l, r, t);
+//   } else if (nums[m] < t) {
+//     l = m + 1;
+//     return binarySeARCH(nums, l, r, t);
+//   }
+
+//   return -1;
+// };
+
+const binarySearch = (
+  numbers: number[],
+  start: number,
+  end: number,
+  target: number
+) => {
+  // console.log('\nnumbers: ', numbers);
+  // console.log('start: ', start);
+  // console.log('end: ', end);
+  // console.log('target: ', target);
+  const idx = start + Math.floor((end - start) / 2);
+  const current = numbers[idx];
+  // console.log('current: ', current);
+
+  if (current === target) return idx;
+  if (end < start) return -1;
+  if (current < target) return binarySearch(numbers, idx + 1, end, target);
+  else if (current > target)
+    return binarySearch(numbers, start, idx - 1, target);
+  else return -1;
+};
+
 for (const testCase of testCases) {
-  const result = binarySearch(testCase.input, testCase.target)
+  const result = binarySearch(
+    testCase.input,
+    0,
+    testCase.input.length - 1,
+    testCase.target
+  );
+
   if (result !== testCase.expected) {
     console.error(
       `Test case failed: Input: ${testCase.input}, Target: ${testCase.target}, Expected: ${testCase.expected}, Result: ${result}`
-    )
+    );
   } else {
     console.log(
       `Test case passed: Input: ${testCase.input}, Target: ${testCase.target}, Result: ${result}`
-    )
+    );
   }
 }
---
+++ quick-sort.ts
@@ -0,0 +1,50 @@
+const swap = (arr: number[], n1Idx: number, n2Idx: number) => {
+  let temp: number = 0;
+  temp = arr[n1Idx];
+  arr[n1Idx] = arr[n2Idx];
+  arr[n2Idx] = temp;
+};
+
+const _swap = (arr: number[], n1Idx: number, n2Idx: number) => {
+  arr[n1Idx] = arr[n1Idx] + arr[n2Idx];
+
+  arr[n2Idx] = arr[n1Idx] - arr[n2Idx];
+  arr[n1Idx] = arr[n1Idx] - arr[n2Idx];
+};
+
+function quickSort(numArr: number[], l: number, h: number) {
+  const partition = (numArr: number[], pivotIdx: number): number => {
+    let i = l + 1;
+    let j = h;
+    const pivot = numArr[pivotIdx];
+
+    while (i < j) {
+      do {
+        i++;
+      } while (numArr[i] < pivot);
+      do {
+        j--;
+      } while (numArr[j] > pivot);
+
+      if (i < j) {
+        swap(numArr, i, j);
+      }
+    }
+    console.log('i: ', i, 'j: ', j, 'pivotIdx: ', pivotIdx, 'numArr: ', numArr);
+    swap(numArr, j, pivotIdx);
+    return j;
+  };
+
+  const pivotIdx = partition(numArr, l);
+
+  if (pivotIdx > h) {
+    console.log('\npivotIdx: ', pivotIdx);
+    quickSort(numArr, l, pivotIdx);
+    quickSort(numArr, pivotIdx + 1, h);
+  }
+}
+const arr = [2, 9, 3, 6, 1, 96];
+
+quickSort(arr, 0, arr.length - 1);
+
+console.log(arr);
